4f1e36119f1dcf7d9db44491140aff9b6bc0c36c
loissm <elloissa.mata@gmail.com>
2023-03-18 21:38:00 -0400

    Formatting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <div class="px-4 py-5 my-5 text-center">
-          <h1 class="display-5 fw-bold">Welcome to my portfolio.</h1>
+        <h1 class="display-5 fw-bold">Welcome to my portfolio.</h1>
       </div>
     </div>
   );
--- src/pages/NoMatch.jsx
+++ src/pages/NoMatch.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 // Render the no match page for invalid urls
 export default function NoMatch() {
-    return (
-      <div>
-        <h2>Nothing to see here!</h2>
-        <p>
-          <Link to="/">Go to the home page</Link>
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
