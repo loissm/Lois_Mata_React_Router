77e01f8ea6c1dc7e0f99cc609937bc6c629be9fa
loissm <elloissa.mata@gmail.com>
2023-03-18 21:49:56 -0400

    Fixed nav links

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,23 +7,23 @@ export default function Layout() {
             share across all the pages on your site, like navigation. */}
             <nav class="navbar navbar-expand-sm bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Lois Mata's Portfolio</a>
+                    <div class="navbar-brand">Lois Mata's Portfolio</div>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-sm-0">
                             <li class="nav-item">
-                                <a class="nav-link" href="#"><NavLink to="/">Home</NavLink></a>
+                                <div class="nav-link"><NavLink to="/">Home</NavLink></div>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#"><NavLink to="/about">About</NavLink></a>
+                                <div class="nav-link"><NavLink to="/about">About</NavLink></div>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#"><NavLink to="/projects">Projects</NavLink></a>
+                                <div class="nav-link"><NavLink to="/projects">Projects</NavLink></div>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#"><NavLink to="/contact">Contact</NavLink></a>
+                                <div class="nav-link"><NavLink to="/contact">Contact</NavLink></div>
                             </li>
                         </ul>
                     </div>
